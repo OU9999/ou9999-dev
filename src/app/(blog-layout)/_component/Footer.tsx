4eb522d79905daec0c2d54f6c1a96b3f22c8e6fc
오유진 <omh232323@gmail.com>
2024-01-22 18:09:01 +0900

    footer and icons
    RSS 기능은 나중에 추가해줘야함

--- src/app/(blog-layout)/_component/Footer.tsx
+++ src/app/(blog-layout)/_component/Footer.tsx
@@ -1,27 +1,33 @@
 import GithubIcon from "@/components/svg/GithubIcon";
+import MailIcon from "@/components/svg/MailIcon";
+import RssIcon from "@/components/svg/RSSIcon";
+import Link from "next/link";
 
 interface IIconBoxProps {
+  link: string;
   icon: JSX.Element;
 }
-const IconBox = ({ icon }: IIconBoxProps) => {
+const IconBox = ({ link, icon }: IIconBoxProps) => {
   return (
-    <div className="w-5 h-5 cursor-pointer fill-black dark:fill-white hover:fill-gradient-end hover:dark:fill-gradient-start">
-      {icon}
-    </div>
+    <Link href={link}>
+      <div className="w-5 h-5 cursor-pointer fill-black dark:fill-white hover:fill-gradient-end hover:dark:fill-gradient-start">
+        {icon}
+      </div>
+    </Link>
   );
 };
 
 const Footer = () => {
   return (
     <div className="w-full mt-20 flex justify-center">
-      <div className="w-full max-w-276 border-t-1 pt-10 pb-5 flex flex-col justify-center items-center">
+      <div className="w-full max-w-276 border-t-1 border-slate-300 dark:border-slate-700 pt-10 pb-5 flex flex-col justify-center items-center">
         <div className="flex space-x-5">
-          <IconBox icon={<GithubIcon />} />
-          <IconBox icon={<GithubIcon />} />
-          <IconBox icon={<GithubIcon />} />
+          <IconBox link="mailto:omh232323@gmail.com" icon={<MailIcon />} />
+          <IconBox link="https://github.com/OU9999" icon={<GithubIcon />} />
+          <IconBox link="/" icon={<RssIcon />} />
         </div>
-        <p className="mt-3 text-xs">
-          2024﹒© OU9999. All Right Reserved. Powered by Next.js﹒Vercel
+        <p className="mt-3 text-xs text-slate-500 dark:text-slate-400">
+          2024﹒© OU9999. Powered by Next.js﹒Vercel
         </p>
       </div>
     </div>
