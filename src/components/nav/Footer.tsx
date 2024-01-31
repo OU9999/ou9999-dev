0928091b34a96266c7dd9e2c7eb718166e400f61
오유진 <omh232323@gmail.com>
2024-01-31 21:22:55 +0900

    [update] footer design
    flex-col => flex-row

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -10,7 +10,7 @@ interface IIconBoxProps {
 const IconBox = ({ link, icon }: IIconBoxProps) => {
   return (
     <Link href={link}>
-      <div className="w-5 h-5 cursor-pointer fill-black dark:fill-white hover:fill-gradient-end hover:dark:fill-gradient-start">
+      <div className="w-4 h-4 md:w-5 md:h-5 cursor-pointer fill-black dark:fill-white hover:fill-gradient-end hover:dark:fill-gradient-start">
         {icon}
       </div>
     </Link>
@@ -20,15 +20,19 @@ const IconBox = ({ link, icon }: IIconBoxProps) => {
 const Footer = () => {
   return (
     <div className="w-full mt-20 flex justify-center border-t-1 border-slate-300 dark:border-slate-700">
-      <div className="w-full max-w-276  pt-10 pb-5 flex flex-col justify-center items-center">
-        <div className="flex space-x-5">
+      <div className="w-full max-w-276 p-5 flex justify-between items-center">
+        <div className="flex flex-col space-y-1  text-slate-500 dark:text-slate-400">
+          <div className="flex text-sm">
+            <p>2024﹒©</p>
+            <p className="font-bold">&nbsp;OU9999</p>
+          </div>
+          <p className="text-xs">Powered by Next.js﹒Vercel</p>
+        </div>
+        <div className="flex space-x-4 md:space-x-5">
           <IconBox link="mailto:omh232323@gmail.com" icon={<MailIcon />} />
           <IconBox link="https://github.com/OU9999" icon={<GithubIcon />} />
           <IconBox link="/" icon={<RssIcon />} />
         </div>
-        <p className="mt-3 text-xs text-slate-500 dark:text-slate-400">
-          2024﹒© OU9999. Powered by Next.js﹒Vercel
-        </p>
       </div>
     </div>
   );
