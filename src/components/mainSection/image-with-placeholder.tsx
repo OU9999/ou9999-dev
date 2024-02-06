fd71f682660d788b25945f5b01355f0ddb8611fb
오유진 <omh232323@gmail.com>
2024-02-06 23:16:44 +0900

    [hotfix] delete dynamic blur
    몇번의 test를 거쳐야함
    아직은 기본 blur 사용해야함

--- src/components/mainSection/image-with-placeholder.tsx
+++ src/components/mainSection/image-with-placeholder.tsx
@@ -1,9 +1,6 @@
-import { getBase64Data } from "@/utils/base64Util";
 import Image, { ImageProps } from "next/image";
 
 const ImageWithPlaceholder: React.FC<ImageProps> = async (props) => {
-  const blurData = await getBase64Data(props.src as string);
-
   return (
     <div className="relative flex flex-col justify-center items-center">
       <Image
@@ -11,7 +8,9 @@ const ImageWithPlaceholder: React.FC<ImageProps> = async (props) => {
         alt={props.alt}
         className="rounded-lg"
         placeholder="blur"
-        blurDataURL={blurData}
+        blurDataURL={
+          "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPce/h4PQAHVALI8GDtfQAAAABJRU5ErkJggg=="
+        }
       />
       {props.alt && (
         <p className="my-1 text-gray-400 dark:text-gray-600 text-sm ">
