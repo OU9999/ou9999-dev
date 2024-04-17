d2786e8e9e575a09f0e1a920717bee2033855902
오유진 <omh232323@gmail.com>
2024-04-17 17:17:55 +0900

    [update] Replicate 후기 업데이트
    1. 후기 문구 추가
    
    2. portfolio 페이지 작업중

--- src/app/(about-layout)/portfolio/page.tsx
+++ src/app/(about-layout)/portfolio/page.tsx
@@ -1,9 +1,18 @@
 import AboutMe from "@/components/mainSection/about-me";
+import { redirect } from "next/navigation";
 
 const PortfolioPage = () => {
+  redirect("/about");
+
   return (
     <>
       <AboutMe />
+      <p>FactorLabs</p>
+      <p>2023.05 ~ 2023.10</p>
+      <p>ou9999-dev.com</p>
+      <p>2024.01 ~</p>
+      <p>ou-playground.com</p>
+      <p>2024.04 ~</p>
     </>
   );
 };
