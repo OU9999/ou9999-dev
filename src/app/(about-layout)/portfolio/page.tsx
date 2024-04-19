8e052b4b9a94b44b7a5b4300803eea2b7e53ca25
오유진 <omh232323@gmail.com>
2024-04-19 22:42:38 +0900

    [update] 23회고 글 내용
    1. 걱정,기대 볼드처리 지움
    
    2. portfolio 페이지 card ui 컴포넌트 제작

--- src/app/(about-layout)/portfolio/page.tsx
+++ src/app/(about-layout)/portfolio/page.tsx
@@ -1,18 +1,35 @@
 import AboutMe from "@/components/mainSection/about-me";
+import PortfolioCard from "@/components/ui/card";
 import { redirect } from "next/navigation";
 
 const PortfolioPage = () => {
   redirect("/about");
+  const arr = new Array(4).fill(1);
 
   return (
     <>
       <AboutMe />
-      <p>FactorLabs</p>
-      <p>2023.05 ~ 2023.10</p>
-      <p>ou9999-dev.com</p>
-      <p>2024.01 ~</p>
-      <p>ou-playground.com</p>
-      <p>2024.04 ~</p>
+
+      <div className="w-full flex flex-col">
+        <p className="text-xl">Contact</p>
+        <p>omh232323@gmail.com</p>
+        <p>blog</p>
+        <p>github</p>
+        <p className="text-xl">경험</p>
+        <p>FactorLabs</p>
+        <p>Front-End Developer</p>
+        <p>2023.05 ~ 2023.10</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-1">
+        {arr.map((_, idx) => (
+          <PortfolioCard
+            key={"cardd" + idx}
+            className="w-auto h-auto flex flex-col"
+          >
+            <p>ou9999-dev.com</p>
+          </PortfolioCard>
+        ))}
+      </div>
     </>
   );
 };
