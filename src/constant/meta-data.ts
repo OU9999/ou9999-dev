c0424e1bd4de40e58e829b000d5d7af0b2b5de60
오유진 <omh232323@gmail.com>
2024-06-05 18:45:24 +0900

    [update] meta-data 설명
    1. 설명 업데이트

--- src/constant/meta-data.ts
+++ src/constant/meta-data.ts
@@ -2,7 +2,8 @@ import { OpenGraph } from "next/dist/lib/metadata/types/opengraph-types";
 import { Twitter } from "next/dist/lib/metadata/types/twitter-types";
 
 const defaultTitle = "ou9999.dev";
-const defaultDescription = "OU9999's blog";
+const defaultDescription =
+  "직접 탐구하고 겪었던 문제점과 개선점을 공유하는 글을 작성하는 블로그입니다.";
 
 export const defaultOpenGraph: OpenGraph = {
   type: "website",
