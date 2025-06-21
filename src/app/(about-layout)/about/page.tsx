7f6de69572e24be34ee97b10614a3248993fc4ac
오유진 <omh232323@gmail.com>
2025-06-21 23:38:42 +0900

    update: 포트폴리오 버튼 비뢀성화

--- src/app/(about-layout)/about/page.tsx
+++ src/app/(about-layout)/about/page.tsx
@@ -1,23 +1,29 @@
 import AboutMe from "@/components/mainSection/about-me";
 import Link from "next/link";
 
+const PortfolioLink = () => {
+  return (
+    <div className="w-full flex justify-center mt-14">
+      <Link href={"/portfolio"}>
+        <div className="rotated_gradient_box font-semibold rounded-md p-[1px] cursor-pointer hover:group group">
+          <div className="flex justify-center items-center w-full bg-content-header-white dark:bg-content-header-black rounded-md px-3 py-1">
+            <p
+              className={`inline-block bg-gradient-to-r from-gradient-start to-gradient-end dark:from-white dark:to-white text-transparent bg-clip-text group-hover:from-violet-500 group-hover:to-teal-500 group-hover:dark:from-gradient-start group-hover:dark:to-gradient-end`}
+            >
+              Portfolio
+            </p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
 const AboutPage = () => {
   return (
     <>
       <AboutMe />
-      <div className="w-full flex justify-center mt-14">
-        <Link href={"/portfolio"}>
-          <div className="rotated_gradient_box font-semibold rounded-md p-[1px] cursor-pointer hover:group group">
-            <div className="flex justify-center items-center w-full bg-content-header-white dark:bg-content-header-black rounded-md px-3 py-1">
-              <p
-                className={`inline-block bg-gradient-to-r from-gradient-start to-gradient-end dark:from-white dark:to-white text-transparent bg-clip-text group-hover:from-violet-500 group-hover:to-teal-500 group-hover:dark:from-gradient-start group-hover:dark:to-gradient-end`}
-              >
-                Portfolio
-              </p>
-            </div>
-          </div>
-        </Link>
-      </div>
+      {/* <PortfolioLink /> */}
     </>
   );
 };
