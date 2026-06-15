5e5a92774e41d449e9f00dc39c155d19161a6a12
오유진 <omh232323@gmail.com>
2026-06-16 00:54:01 +0900

    style: 기본 OG 이미지 변경

--- src/constant/meta-data.ts
+++ src/constant/meta-data.ts
@@ -4,13 +4,14 @@ import { Twitter } from "next/dist/lib/metadata/types/twitter-types";
 const defaultTitle = "ou9999.dev";
 const defaultDescription =
   "직접 탐구하고 겪었던 문제점과 개선점을 공유하는 글을 작성하는 블로그입니다.";
+const defaultOpenGraphImage = "/imgs/openGraph/default-og.png";
 
 const defaultOpenGraph: OpenGraph = {
   type: "website",
   title: defaultTitle,
   description: defaultDescription,
   images: {
-    url: "/imgs/openGraph/ovo3.png",
+    url: defaultOpenGraphImage,
     width: 1200,
     height: 630,
   },
@@ -21,7 +22,7 @@ const defaultTwitter: Twitter = {
   site: "@OU9999",
   title: defaultTitle,
   description: defaultDescription,
-  images: ["/imgs/openGraph/ovo3.png"],
+  images: [defaultOpenGraphImage],
 };
 
 export { defaultOpenGraph, defaultTwitter };
