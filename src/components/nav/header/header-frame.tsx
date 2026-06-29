2b672431e4444d489d89b3acab2c7339e1552a56
오유진 <omh232323@gmail.com>
2026-06-29 20:25:57 +0900

    style: SVG 헤더 디자인 적용

---
+++ src/components/nav/header/header-frame.tsx
@@ -0,0 +1,20 @@
+const HeaderFrame = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      className="pointer-events-none absolute left-0 top-[-1px] h-[64px] w-full overflow-visible md:top-[-2px] md:h-[101px] md:w-[1435px]"
+      focusable="false"
+      preserveAspectRatio="none"
+      viewBox="0 0 1435 101"
+    >
+      <image
+        href="/imgs/header/header-frame-reference.svg"
+        height="101"
+        preserveAspectRatio="none"
+        width="1435"
+      />
+    </svg>
+  );
+};
+
+export { HeaderFrame };
